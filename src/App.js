f8828e2f458395e401f60ee04b1f7b8b8eabbab8
alachhman <33943685+alachhman@users.noreply.github.com>
2021-01-30 01:14:52 -0500

    based commit

--- src/App.js
+++ src/App.js
@@ -5,22 +5,40 @@ import './App.css';
 function App() {
     const [words, setWords] = useState([]);
     const fetchWords = async () => {
-        setWords(await fetch("http://api.datamuse.com/words?sp=a*&md=p")
+        // let chosenLetter = getRandomLetter();
+        let chosenLetter = "u";
+        let apiCall = "http://api.datamuse.com/words?sp=" + chosenLetter + "*&md=p";
+        console.log(chosenLetter, apiCall);
+        setWords(await fetch(apiCall)
             .then(response => response.json())
-            .then(json => json))
+            .then(json => {
+                json.filter(x => x.tags.includes("v"))
+            })
+        )
     };
     useEffect(() => {
         document.title = "X Your Mom";
         fetchWords().then(x => x)
         console.log(words)
-    }, []);
+    }, [words]);
     return (
         <div className="App">
             <header className="App-header">
-                {words.length > 0 ? words[0].word : "None"}
+                I'm going to {words.length > 0
+                ? words[getRandomInt(words.length)].word
+                : "None"} your mom.
             </header>
         </div>
     );
 }
 
 export default App;
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+function getRandomLetter() {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz"
+    return alphabet[Math.floor(Math.random() * alphabet.length)]
+}
